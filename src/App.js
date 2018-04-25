08447f5a0b4fcafa368355b16a369a922bcdfcf7
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-04-25 15:18:55 -0500

    Add tasks to app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const todos = [{
+  task: 'learn react',
+  isCompleted: false
+}
+{
+  task: 'make dinner',
+  isCompleted: true
+}
+
+];
 class App extends Component {
   render() {
     return (
