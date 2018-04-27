4dfa56b7617f5b8c4f96ea90a36baee28d4b4d55
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-04-26 19:38:59 -0500

    Add isEditing state with if and else

--- src/todos-list-item.js
+++ src/todos-list-item.js
@@ -1,15 +1,37 @@
 import React, { Component } from 'react';
 
 export class TodosListItem extends Component {
+  constructor (props){
+    super(props);
+
+    this.state = {
+isEditing: false
+    };
+  }
+  renderActionSection(){
+    if(this.state.isEditing) {
+      return(
+<button onClick={this.onEditClick.bind(this)}>Save</button>
+          <button>Cancel</button>
+      );
+    }
+    return(
+<button onClick={this.onEditClick.bind(this)}>Edit</button>
+          <button>Delete</button>
+    );
+  }
   render() {
     return (
         <tr>
          <td>{this.props.task}</td>
          <td>
-          <button>Edit</button>
+          <button onClick={this.onEditClick.bind(this)}>Edit</button>
           <button>Delete</button>
          </td>
         </tr>
     );
+  }
+  onEditClick(){
+    this.setState({ isEditing: true});
   }
 }
