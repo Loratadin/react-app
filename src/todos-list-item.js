cc462b949c561bb1fbc23006fb39d724c3558e4e
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-04-26 19:49:38 -0500

    Set up onCancelClick

--- src/todos-list-item.js
+++ src/todos-list-item.js
@@ -11,27 +11,31 @@ isEditing: false
   renderActionSection(){
     if(this.state.isEditing) {
       return(
-<button onClick={this.onEditClick.bind(this)}>Save</button>
-          <button>Cancel</button>
+        <td>
+<button>Save</button>
+          <button onClick={this.onCancelClick.bind(this)}>Cancel</button>
+          </td>
       );
     }
     return(
+      <td>
 <button onClick={this.onEditClick.bind(this)}>Edit</button>
           <button>Delete</button>
+          </td>
     );
   }
   render() {
     return (
         <tr>
          <td>{this.props.task}</td>
-         <td>
-          <button onClick={this.onEditClick.bind(this)}>Edit</button>
-          <button>Delete</button>
-         </td>
+         {this.renderActionSection()}
         </tr>
     );
   }
   onEditClick(){
     this.setState({ isEditing: true});
+  }
+  onCancelClick(){
+    this.setState({ isEditing: false});
   }
 }
