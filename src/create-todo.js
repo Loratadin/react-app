e352acaac9d52abbba1e04459619cec4246f96c7
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-04-28 07:26:28 -0500

    create error message

--- src/create-todo.js
+++ src/create-todo.js
@@ -2,12 +2,26 @@ import React, { Component } from 'react';
 import _ from 'lodash';
 
 export class CreateTodo extends Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      error: null
+    };
+  }
+
+  renderError() {
+if (!this.state.error) { return null; }
+
+return <div style={{ color: 'red'}}>{this.state.Error}</div>
+  }
  
   render() {
     return (
       <form onSubmit={this.handleCreate.bind(this)}>
        <input type="text" placeholder="What do I need to do?" ref="createInput"/>
        <button>Create</button>
+       {this.renderError()}
       </form>
     );
   }
@@ -18,6 +32,11 @@ const createInput = this.refs.createInput;
 const task = createInput.value;
 const validateInput = this.validateInput(task);
 
+if (validateInput) {
+  this.setState({ error: validateInput });
+  return;
+}
+this.setState({ error: null });
 this.props.createTask(task);
 this.refs.createInput.value = '';
   }
